01f279d5e689a277a6a71f126648c2c45f39d703
JisooHahn <luna99954@gmail.com>
2025-02-28 16:41:58 +0900

    16:41 커밋 v.2

--- src/main/java/com/app/temp/controller/main/IntroduceController.java
+++ src/main/java/com/app/temp/controller/main/IntroduceController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main/*")
+@RequestMapping("/intro/*")
 @Slf4j
-public class MainPageController {
+public class IntroduceController {
 
-    @GetMapping("introduce")
-    public void introduce() {
+    @GetMapping("/")
+    public String introduce() {
+        return "main/introduce";
     }
 }
